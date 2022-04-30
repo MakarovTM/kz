e8ea1725a1229f47e4c8b74184b809f30044c4b4
MakarovTM <dev@tmaster.su>
2022-04-30 18:27:20 +0300

    local: 2022-04-30

--- processFz44Server/_modules/servicesFiles/ProcessFileXml.py
+++ processFz44Server/_modules/servicesFiles/ProcessFileXml.py
@@ -1,5 +1,7 @@
 from lxml import etree
 
+from _modules.servicesProgram.ProgramLogger import ProgramLogger
+
 
 class ProcessFileXml:
 
@@ -14,15 +16,26 @@ class ProcessFileXml:
             Автор     : Макаров Алексей
             Описание  : Магический метод, выполняемый при инициализации объекта
             Получаем  : {
-                            varType: bytes,
-                            varName: xmlFileContent,
-                            varDesc: Буфер байтов в памяти aka содержимое файла
+                            varType:    bytes,
+                            varName:    xmlFileContent,
+                            varDesc:    Буфер байтов в памяти
                         }
         """
 
-        self._fileContent = etree.fromstring(xmlFileContent)
+        self._logger = ProgramLogger()
 
-    def __cleanUpNameSpaces(self) -> None:
+        try:
+            self._fileContent = etree.fromstring(xmlFileContent)
+            if self.__cleanUpNameSpaces() != 0:
+                self._logger.logError(
+                    "Произошла ошибка при удалении пространства имен"
+                )
+        except Exception:
+            self._logger.logError(
+                "При инициализации класса был передан некорректный файл"
+            )
+
+    def __cleanUpNameSpaces(self) -> int:
 
         """
             Автор     : Макаров Алексей
@@ -30,6 +43,68 @@ class ProcessFileXml:
         """
 
         for elem in self._fileContent.getiterator():
-            if not isinstance(elem, (etree._Comment, etree._ProcessingInstruction)):
+            if not isinstance(
+                    elem, (etree._Comment, etree._ProcessingInstruction)):
                 elem.tag = etree.QName(elem).localname
         etree.cleanup_namespaces(self._fileContent)
+
+        return 0
+
+    def essenceDataWithXpath(self, essenceStructure: dict) -> str:
+
+        """
+            Автор     : Макаров Алексей
+            Описание  : Выполнение извлечения
+                        данных из XML файла согласно переданной структуре
+            Получаем  : {
+                            varType:    Dict,
+                            varName:    essenceStructure,
+                            varDesc:    Структура,
+                                        описывающая извлекаемые данные
+                            varTree:    {
+                                            varType:    Bool,
+                                            varName:    multi,
+                                            varDesc:    Искомое значение
+                                                        содержится в потомках
+
+                                            varType:    Dict,
+                                            varName:    xPath
+                                            varDesc:    Словарь со знач. xPath
+                                                        для извлечения данных
+                                                        из узлов документа
+
+                                            varType:    Dict Elem,
+                                            varName:    parent,
+                                            varDesc:    Значение единое:
+                                                            xPath узла
+                                                        Значение множеств:
+                                                            xPath корн. узла
+
+                                            varType:    Dict Elem,
+                                            varName:    nested,
+                                            varDesc:    Значение множеств:
+                                                            xPath узла
+
+                                            varType:    Str,
+                                            varName:    default,
+                                            varDesc:    Значение для
+                                                        узла по умолчанию
+                                        }
+                        }
+            Возвращаем: {
+                            varType:    Str,
+                            varDesc:    Полученное значение по заданному ключу
+                        }
+        """
+
+        essencedValues = []
+
+        if not essenceStructure["multi"]:
+            essencedElem = self.\
+                _fileContent.find(essenceStructure["xPath"]["parent"])
+            if essencedElem is not None:
+                essencedValues.append(essencedElem.text)
+            else:
+                essencedValues.append(essenceStructure["default"])
+
+        return "; ".join(set(list(map(str, essencedValues))))
